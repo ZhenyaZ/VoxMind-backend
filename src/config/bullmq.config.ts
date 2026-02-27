8fec8663a08fdff59a1212c138a715d64a473ae1
ZhenyaZ <darkest2004@gmail.com>
2026-02-27 20:58:22 +0100

    update bullmq config

--- src/config/bullmq.config.ts
+++ src/config/bullmq.config.ts
@@ -5,6 +5,7 @@ export const BullMQConfig = BullModule.forRoot({
     host: process.env.ENV === 'prod' ? process.env.REDIS_HOST_PROD : process.env.REDIS_HOST_DEV,
     username: process.env.ENV === 'prod' ? process.env.REDIS_USER_PROD : '',
     password: process.env.ENV === 'prod' ? process.env.REDIS_PWD_PROD : '',
+    url: process.env.ENV === 'prod' ? process.env.REDIS_URL_PROD : undefined,
     port: Number(process.env.REDIS_PORT),
   },
 });
