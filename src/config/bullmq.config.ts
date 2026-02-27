8b75f4b249d805c2540564d9a85ea9e2c7da970c
ZhenyaZ <darkest2004@gmail.com>
2026-02-27 21:08:22 +0100

    updat ed bullmq config

--- src/config/bullmq.config.ts
+++ src/config/bullmq.config.ts
@@ -3,8 +3,8 @@ import { BullModule } from '@nestjs/bullmq';
 export const BullMQConfig = BullModule.forRoot({
   connection: {
     // host: process.env.ENV === 'prod' ? process.env.REDIS_HOST_PROD : process.env.REDIS_HOST_DEV,
-    username: process.env.ENV === 'prod' ? process.env.REDIS_USER_PROD : '',
-    password: process.env.ENV === 'prod' ? process.env.REDIS_PWD_PROD : '',
+    // username: process.env.ENV === 'prod' ? process.env.REDIS_USER_PROD : '',
+    // password: process.env.ENV === 'prod' ? process.env.REDIS_PWD_PROD : '',
     url: process.env.ENV === 'prod' ? process.env.REDIS_URL_PROD : undefined,
     port: Number(process.env.REDIS_PORT),
   },
